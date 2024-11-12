57a3b4171efdb48265e057ab825625b1a144850b
Hardik Makkar <hardikmakkar27@gmail.com>
2024-11-12 21:29:56 +0530

    Error is BKL

--- src/app/dsa/dsa.component.ts
+++ src/app/dsa/dsa.component.ts
@@ -2,75 +2,85 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/authService';
 import { Route, Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 interface Data {
-  topicName : string;
-  topicArticleLink : string;
-  topicProblemLink : string;
-  topicChecked : boolean;
+  topicName: string;
+  topicArticleLink: string;
+  topicProblemLink: string;
+  topicChecked: boolean;
 }
 
-interface basicData{
-  Article : string,
-  Difficulty : string,
-  Practice : any,
-  Video : string
+interface basicData {
+  Article: string,
+  Difficulty: string,
+  Practice: any,
+  Video: string
 }
 @Component({
   selector: 'app-dsa',
   templateUrl: './dsa.component.html',
   styleUrls: ['./dsa.component.css']
 })
-export class DsaComponent  implements OnInit{
-  constructor(private http : HttpClient , 
-    private auth : AuthService,
-    private route : Router
-  ){}
-  dataWant : any;
-  dataFetch  :any;
-  labelForBasicData : any;
-  labelForSTLData : any;
-  labelForPatternData  :any;
+export class DsaComponent implements OnInit {
+  constructor(private http: HttpClient,
+    private auth: AuthService,
+    private route: Router,
+    private afAuth: AngularFireAuth
+  ) { }
+  dataWant: any;
+  dataFetch: any;
+  labelForBasicData: any;
+  labelForSTLData: any;
+  labelForPatternData: any;
   labelForRec_ProbData: any;
-  labelForMath_ProbData : any;
-  labelForHash_ProbData  : any;
-  ngOnInit(): void {  
-    if(!!this.auth.user$){
-      alert('Sign In to continue');
+  labelForMath_ProbData: any;
+  labelForHash_ProbData: any;
+  ngOnInit(): void {
+   this.afAuth.authState.subscribe(user =>{
+    if(!user){
+      alert('Sign in to continue');
       this.route.navigate(['/home']);
     }
-    const url = 'https://edunavigator-dc324-default-rtdb.firebaseio.com/.json';
-    this.http.get(url).subscribe(
-      (response) => {
-       this.dataFetch =  Object.values(response);
-       this.dataWant = this.dataFetch[1];
-       this.basicData = Object.values(this.dataWant);
-       console.log(this.basicData);
-       this.labelForRec_ProbData = Object.keys(this.basicData[2]);
-       this.labelForMath_ProbData = Object.keys(this.basicData[1]);
-       this.labelForHash_ProbData = Object.keys(this.basicData[0]);
-       this.labelForPatternData = Object.keys(this.basicData[3]);
-       this.labelForBasicData = Object.keys(this.basicData[5]);
-       this.labelForSTLData = Object.keys(this.basicData[4]);
-       this.patternData = Object.values(this.basicData[3]);
-       this.stlData = Object.values(this.basicData[4]);
-       this.recData  = Object.values(this.basicData[2]);
-       this.mathData  = Object.values(this.basicData[1]);
-       this.hashData  = Object.values(this.basicData[0]);
-       this.basicData = Object.values(this.basicData[5]); 
-      }
-    );
+    if(user){
+     user.getIdToken().then( token => {
+      console.log(user);
+      const url = `https://edunavigator-dc324-default-rtdb.firebaseio.com/.json?auth=${token}`;
+      this.http.get(url).subscribe(
+        (response) => {
+          this.dataFetch = Object.values(response);
+          this.dataWant = this.dataFetch[1];
+          this.basicData = Object.values(this.dataWant);
+          console.log(this.basicData);
+          this.labelForRec_ProbData = Object.keys(this.basicData[2]);
+          this.labelForMath_ProbData = Object.keys(this.basicData[1]);
+          this.labelForHash_ProbData = Object.keys(this.basicData[0]);
+          this.labelForPatternData = Object.keys(this.basicData[3]);
+          this.labelForBasicData = Object.keys(this.basicData[5]);
+          this.labelForSTLData = Object.keys(this.basicData[4]);
+          this.patternData = Object.values(this.basicData[3]);
+          this.stlData = Object.values(this.basicData[4]);
+          this.recData = Object.values(this.basicData[2]);
+          this.mathData = Object.values(this.basicData[1]);
+          this.hashData = Object.values(this.basicData[0]);
+          this.basicData = Object.values(this.basicData[5]);
+        }
+      );
+     })
+    }
+   })
   }
-  content: string | null = null;
-  basicData : basicData[] = [];
-  stlData : basicData[] = [];
-  hashData : basicData[] = [];
-  patternData : basicData[] = [];
-  recData  : basicData[] = [];
-  mathData : basicData[] = [];
-  
-  
-  toggleContent(section: string) {
-    this.content = this.content === section ? null : section;
-  }
+
+content: string | null = null;
+basicData: basicData[] = [];
+stlData: basicData[] = [];
+hashData: basicData[] = [];
+patternData: basicData[] = [];
+recData: basicData[] = [];
+mathData: basicData[] = [];
+
+
+toggleContent(section: string) {
+  this.content = this.content === section ? null : section;
+}
 }
